f6ce4721c283a32764292915cbf633c05b651cff
Josh Taylor <josh@joshtaylordesign.com>
2023-02-20 12:54:58 -0800

    next js structure

---
+++ space-site/src/components/HeadNav.js
@@ -0,0 +1,5 @@
+export function HeadNav() {
+    return (
+    <p>HeadNav</p>
+    )
+  }
---
+++ space-site/src/pages/Destination.js
@@ -0,0 +1,10 @@
+
+import { HeadNav } from '../components/HeadNav.js';
+
+
+export default function Destination() {
+    return <div>
+      <h1>destination</h1>
+      <HeadNav />
+      </div>
+  }
