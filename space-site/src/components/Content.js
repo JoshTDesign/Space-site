76e747bf92d3fb4362187387a2db8b6f4275a72f
Josh Taylor <josh@joshtaylordesign.com>
2023-02-26 06:57:41 -0800

    css grid

--- space-site/src/components/Content.js
+++ space-site/src/components/Content.js
@@ -6,7 +6,7 @@ import { Info } from './Info';
 
 export function Content() {
     return (
-    <div style={{"border":"1px solid red"}}>
+    <div id="main">
       <Graphic />
       <Info />
     </div>
--- space-site/src/components/Graphic.js
+++ space-site/src/components/Graphic.js
@@ -1,5 +1,5 @@
 export function Graphic() {
     return (
-    <p  style={{"border":"1px solid red"}}>Graphic</p>
+    <p>Graphic</p>
     )
   }
